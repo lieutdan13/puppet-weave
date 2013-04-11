92856b9fbe79b953d1bc59dc6fe404d2993e32a5
Dan Schaefer <dan@schaeferzone.net>
2013-04-10 20:46:05 -0400

    Adding an uninstall class

---
+++ manifests/uninstall.pp
@@ -0,0 +1,33 @@
+class weave::uninstall (
+	$java   = true,
+	$weave  = true,
+	$path   = "/opt/weave",
+	$config = true,
+	$git    = false
+) {
+
+	if ( $git ) {
+		require git::params
+
+		if ! defined(Package[$git::params::package]) {
+			package{$git::params::package: ensure => absent}
+		}
+
+		package{$git::params::svn_package: ensure => absent}
+		package{$git::params::gui_package: ensure => absent}
+	}
+
+	if ( $java ) {
+		file { "/tmp/jdk-8.tar.gz": ensure  => absent }
+		file { "/opt/jdk1.8.0": ensure  => absent }
+	}
+
+	if ( $weave ) {
+		file { $path: ensure  => absent }
+	}
+
+	if ( $config ) {
+		file { "/etc/weave.conf": ensure  => absent }
+	}
+
+}
